7e86d45551fda61cc5348ed925494a8b7e6e0633
shihao <pk111100@gmail.com>
2016-08-01 15:26:49 +0800

    修正刪除後頁面跳轉問題

--- application/controllers/backend/Question_option.php
+++ application/controllers/backend/Question_option.php
@@ -53,6 +53,7 @@ class Question_option extends CI_Controller {
 	{
 		// 寫入資料庫
 		$input = array(
+			'correct'=> $this->input->post('correct'),
 			'description' => $this->input->post('description'),
 			'updated_at' => date('Y-m-d H:i:s'),  
 		);
@@ -89,12 +90,15 @@ class Question_option extends CI_Controller {
 
 		$panel= 21;
 		$row=select_submenu($panel);
-		redirect($row["modifylink"]."&mpanel=".$panel."&id=".$this->input->post('question_id'), 'refresh');
+		redirect(
+			$row["modifylink"]."&mpanel=".$panel."&id=".$this->input->post('question_id'), 
+			'refresh');
 	}
 
 	public function delete_detail() {
 		// 寫入資料庫
 		$input = array(
+			'display' => 0,
 			'recover' => 1, 
 			'updated_at' => date('Y-m-d H:i:s'), 
 		);
@@ -104,14 +108,37 @@ class Question_option extends CI_Controller {
 		flashSuccess('刪除資料成功。');
 
 		// 導回原本的頁面
-		$question_option =$this->db->get_where('question_option', array('id' => $this->input->get("id"),))->row_array();
-		$question =$this->db->get_where('question', array('question_id' => $question_option['question_id'],))->row_array();
+		$question_option =$this->db->get_where('question_option', array(
+			'id' => $this->input->get("id"),
+			))->row_array();
+
+		$question =$this->db->get_where('question', array(
+			'id' => $question_option['question_id'],
+			))->row_array();
+		
 		$panel= 21;
 		$row=select_submenu($panel);
-		redirect($row["modifylink"]."&mpanel=".$panel."&id=".$question['id'], 'refresh');
+		redirect($row["modifylink"]."&id=".$question_option['question_id'], 'refresh');
 	}
 
 
+	public function correct()
+	{
+		$id = $this->input->get("id");
+		$correct_static = $this->input->get("correct_static");
+		if($correct_static=='open'){
+			$show=0;
+		}else{
+			$show=1;
+		}
+		$input=array(
+			'correct' => $show,
+			'created_at' => date('Y-m-d H:i:s'),
+		);
+
+		$this->db->where('id', $id);
+		$this->db->update('question_option', $input); 
+	}
 
 
 
