9a2b997e6912aa69c900fa22c4e6cdda64c84bda
shihao <pk111100@gmail.com>
2016-08-01 15:30:00 +0800

    修正頁面跳轉位置

--- application/views/backend/option_modify.php
+++ application/views/backend/option_modify.php
@@ -2,11 +2,13 @@
 include("layout/modify_partials.php");
 include("layout/fileupload_partials.php");
 // 取出索引欄位中的資料值
+
 $id=$this->input->get("id");
-// p($id);
-$row=$this->db->get_where('question_option', array('id' => $id,))->row_array();
-// p($this->db->last_query())
-// p($row)
+$row=$this->db->get_where('question_option', array(
+	'id' => $id,
+	))->row_array();
+
+$question_id = $row['question_id'];
 
 ?>
 
@@ -36,7 +38,6 @@ echo $this->form_builder->build_form_horizontal(
 	    array(
 	        'id' => 'question_id',
 	        'type' => 'hidden',
-	        'value' => $this->input->get('question_id')
 	    ),
 	    array(
 	        'id' => 'description',
@@ -45,15 +46,27 @@ echo $this->form_builder->build_form_horizontal(
 	    ),
 	    array(
 	        'id' => 'correct',
+	        'type' => 'dropdown',
 	        'label' => '是否正確:',
-	        'class' => 'required',
-	        'value' => $this->input->get('correct')
-	    )
+	        'class' => 'required select',
+	        'options' => array( 0=>'不正確', 1 =>'正確')
+	    ),
 ), $row);
 
-// End Button
+
+
+
+$panel = 21;
+$form_end_button = '
+<div class="form-actions text-right">
+	<input type="button" value="回前一頁" class="btn btn-warning" onclick="window.location.href = \''.site_url('backend/page?mpanel=').$panel."&id=".$question_id.'\'">
+	<input type="submit" value="確認" class="btn btn-primary"
+		onclick=""
+	>
+</div>';
+// End ButtonP
 echo $form_end_button;
 // End Form
-echo $this->form_builder->close_form();;
+echo $this->form_builder->close_form();
 ?>
 <!-- 表單結束 -->
